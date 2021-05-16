672bcdce82c107f535a8791f841ceaac76281932
saad277 <saadsheikh27@gmail.com>
2021-05-16 23:23:02 +0500

    home screen

---
+++ lib/model/user.dart
@@ -0,0 +1,9 @@
+class User {
+  String name;
+  String city;
+
+  User(String name, String city) {
+    this.name = name;
+    this.city = city;
+  }
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import 'package:prov/model/user.dart';
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  String _name;
+  String _city;
+
+  List<User> userList = [];
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      appBar: AppBar(
+        title: Text(
+          "Provider Demo",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: SingleChildScrollView(
+          padding: EdgeInsets.all(32),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[],
+            ),
+          )),
+    );
+  }
+}
